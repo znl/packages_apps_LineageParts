2fa50b36d207fd9ada4f9c7aad2b2ea9df79b00b
Michael Bestas <mkbestas@lineageos.org>
2018-01-23 23:03:12 +0200

    LineageParts: Add deviceSupportsFlashLight util
    * It used to live in lineage-sdk QSUtils which is now being removed
    * Move it here since ButtonSettings seems to be the only active user
    
    Change-Id: I085c435181bbfe961029dca72e060363809c1ce0

--- src/org/lineageos/lineageparts/input/ButtonSettings.java
+++ src/org/lineageos/lineageparts/input/ButtonSettings.java
@@ -45,7 +45,6 @@ import org.lineageos.lineageparts.R;
 import org.lineageos.lineageparts.SettingsPreferenceFragment;
 import org.lineageos.lineageparts.utils.DeviceUtils;
 import org.lineageos.lineageparts.utils.TelephonyUtils;
-import org.lineageos.internal.util.QSUtils;
 import org.lineageos.internal.util.ScreenType;
 
 import static org.lineageos.internal.util.DeviceKeysConstants.*;
@@ -233,7 +232,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                 powerCategory.removePreference(mPowerEndCall);
                 mPowerEndCall = null;
             }
-            if (!QSUtils.deviceSupportsFlashLight(getActivity())) {
+            if (!DeviceUtils.deviceSupportsFlashLight(getActivity())) {
                 powerCategory.removePreference(mTorchLongPressPowerGesture);
                 powerCategory.removePreference(mTorchLongPressPowerTimeout);
             }

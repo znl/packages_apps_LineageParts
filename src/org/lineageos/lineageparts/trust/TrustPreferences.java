LineageOS/android_packages_apps_LineageParts

LineageParts: Complete and correct SMS limits port
* Add missing option entries from previous versions
* Correct string, the SMS limit is for 1 minute since jb 4.2
* Use improved strings from previous versions

Change-Id: I78e9fe9c2f0cb9c934607300221be0335c904d49

src/org/lineageos/lineageparts/trust/TrustPreferences.java
@@ -206,9 +206,17 @@ public class TrustPreferences extends SettingsPreferenceFragment {
         return true;
     }
 
+    private void updateSmsSecuritySummary(int selection) {
+        String message = selection > 0
+                ? getContext().getString(R.string.sms_security_check_limit_summary, selection)
+                : getContext().getString(R.string.sms_security_check_limit_summary_none);
+        mSmsLimitPref.setSummary(message);
+    }
+
     private boolean onSmsLimitChanged(Integer value) {
         Settings.Global.putInt(getContext().getContentResolver(),
                 Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, value);
+        updateSmsSecuritySummary(value);
         return true;
     }
 

znl/packages_apps_LineageParts

src/org/lineageos/lineageparts/trust/TrustPreferences.java
@@ -206,9 +206,18 @@ public class TrustPreferences extends SettingsPreferenceFragment {
         return true;
     }
 
+    private void updateSmsSecuritySummary(int selection) {
+        String value = String.valueOf(selection);
+        String message = selection > 0
+                ? getContext().getString(R.string.sms_security_check_limit_summary, value)
+                : getContext().getString(R.string.sms_security_check_limit_summary_none);
+        mSmsLimitPref.setSummary(message);
+    }
+
     private boolean onSmsLimitChanged(Integer value) {
         Settings.Global.putInt(getContext().getContentResolver(),
                 Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, value);
+        updateSmsSecuritySummary(value);
         return true;
     }
 